af26b7ef520de17fbd026609d19b49a8677bd4fd
Антон Шмидт <schmidt.ajax@gmail.com>
2022-03-21 22:56:23 +0700

    WIP

--- main.cpp
+++ main.cpp
@@ -1,13 +1,46 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <vector>
+#include <tuple>
 
 using namespace std;
 
 class Solution {
  public:
-  int nearestValidPoint(int x, int y, vector<vector<int>> &points) {
-    // FIXME
+  struct Point {
+    int x;
+    int y;
+
+    bool operator==(const Point& other) const {
+      return tie(x, y) == tie(other.x, other.y);
+    }
+  };
+
+  int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+    vector<Point> sorted;
+    for (const auto& p: points) {
+      sorted.push_back({ p[0], p[1] });
+    }
+    auto comp = [x, y](const Point& lhs, const Point& rhs) {
+      const int d1 = abs(lhs.x - x) + abs(lhs.y - y);
+      const int d2 = abs(rhs.x - x) + abs(rhs.y - y);
+      return d1 < d2;
+    };
+    sort(sorted.begin(), sorted.end(), comp);
+    auto iter = find_if(sorted.begin(), sorted.end(), [x, y](const Point& p) {
+      return p.x == x || p.y == y;
+    });
+    if (sorted.end() != iter) {
+      const Point& found = *iter;
+      for (int i = 0; i < points.size(); ++i) {
+        const auto& p = points[i];
+        if (found == Point{p[0], p[1]}) {
+          return i;
+        }
+      }
+    }
+
     return -1;
   }
 };
